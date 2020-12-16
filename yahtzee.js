d796b382c2ca88e6aec70c70cf1db75a79575270
Chris Wolfe <Chrislw324@gmail.com>
2020-12-16 17:45:18 -0500

    score totaled but has issue

--- yahtzee.js
+++ yahtzee.js
@@ -6,7 +6,10 @@ let diceFourHeld = false;
 let diceFiveHeld = false;
 let numRoll = 0;
 let remainingRolls = 3;
-
+const fullHouse = 25;
+const smallStraight = 30;
+const largeStraight = 40;
+const yahtzee = 50;
 
 function newGame() {
   numRoll = 0;
@@ -24,11 +27,27 @@ function newGame() {
     dieFace.style.backgroundColor = "tomato";
     dieFace.innerHTML = null;
   }
+  document.getElementById("onesScore").innerHTML = null;
+  document.getElementById("twosScore").innerHTML = null;
+  document.getElementById("threesScore").innerHTML = null;
+  document.getElementById("foursScore").innerHTML = null;
+  document.getElementById("fivesScore").innerHTML = null;
+  document.getElementById("sixesScore").innerHTML = null;
+  document.getElementById("subtotalScore").innerHTML = null;
+  document.getElementById("bonusScore").innerHTML = null;
+  document.getElementById("uppertotalScore").innerHTML = null;
+  document.getElementById("3ofakindScore").innerHTML = null;
+  document.getElementById("4ofakindScore").innerHTML = null;
+  document.getElementById("fullhouseScore").innerHTML = null;
+  document.getElementById("smallstraightScore").innerHTML = null;
+  document.getElementById("largestraightScore").innerHTML = null;
+  document.getElementById("chanceScore").innerHTML = null;
+  document.getElementById("yahtzeeScore").innerHTML = null;
+  document.getElementById("lowertotalScore").innerHTML = null;
+  document.getElementById("totalScore").innerHTML = null;
   let resetRolls = document.getElementById("remainingRolls");
   resetRolls.innerHTML = remainingRolls;
 }
-
-
 
 function rollDice() {
   numRoll++;
@@ -39,8 +58,8 @@ function rollDice() {
   let rollCounter = document.getElementById("remainingRolls");
   rollCounter.innerHTML = remainingRolls;
   if (diceOneHeld === false) {
-      var num1 = (Math.floor(Math.random() * 6) + 1);
-      displayDie(num1, "dieOne");
+    var num1 = (Math.floor(Math.random() * 6) + 1);
+    displayDie(num1, "dieOne");
   }
   if (diceTwoHeld === false) {
     var num2 = (Math.floor(Math.random() * 6) + 1);
@@ -51,13 +70,136 @@ function rollDice() {
     displayDie(num3, "dieThree");
   }
   if (diceFourHeld === false) {
-  let num4 = (Math.floor(Math.random() * 6) + 1);
-  displayDie(num4, "dieFour");
+    var num4 = (Math.floor(Math.random() * 6) + 1);
+    displayDie(num4, "dieFour");
   }
   if (diceFiveHeld === false) {
-    let num5 = (Math.floor(Math.random() * 6) + 1);
+    var num5 = (Math.floor(Math.random() * 6) + 1);
     displayDie(num5, "dieFive");
   }
+
+  //^^^^^^^^^Start of Scoring Section^^^^^^^^^^^^^^
+  if (remainingRolls === 0) {
+
+    let finalRoll = [];
+    finalRoll.push(num1, num2, num3, num4, num5);
+    let diceTotal = (num1 + num2 + num3 + num4 + num5);
+    let numOfOnes = 0;
+    let numOfTwos = 0;
+    let numOfThrees = 0;
+    let numOfFours = 0;
+    let numOfFives = 0;
+    let numOfSixes = 0;
+    for (let i = 0; i < 5; i++) {
+      if (finalRoll[i] === 1) {
+        numOfOnes++;
+      }
+      else if (finalRoll[i] === 2) {
+        numOfTwos++;
+      }
+      else if (finalRoll[i] === 3) {
+        numOfThrees++;
+      }
+      else if (finalRoll[i] === 4) {
+        numOfFours++;
+      }
+      else if (finalRoll[i] === 5) {
+        numOfFives++;
+      }
+      else {
+        numOfSixes++;
+      }
+    }
+    
+    //upper section scoring
+    document.getElementById("onesScore").innerHTML = (numOfOnes * 1);
+    document.getElementById("twosScore").innerHTML = (numOfTwos * 2);
+    document.getElementById("threesScore").innerHTML = (numOfThrees * 3);
+    document.getElementById("foursScore").innerHTML = (numOfFours * 4);
+    document.getElementById("fivesScore").innerHTML = (numOfFives * 5);
+    document.getElementById("sixesScore").innerHTML = (numOfSixes * 6);
+
+
+    //three of a kind
+    if (numOfOnes > 2 || numOfTwos > 2 || numOfThrees > 2 || numOfFours > 2 || numOfFives > 2 || numOfSixes > 2) {
+      document.getElementById("3ofakindScore").innerHTML = diceTotal;
+    }
+    else {
+      document.getElementById("3ofakindScore").innerHTML = "0";
+    }
+
+    //four of a kind
+    if (numOfOnes > 3 || numOfTwos > 3 || numOfThrees > 3 || numOfFours > 3 || numOfFives > 3 || numOfSixes > 3) {
+      document.getElementById("4ofakindScore").innerHTML = diceTotal;
+    }
+    else {
+      document.getElementById("4ofakindScore").innerHTML = "0";
+    }
+
+    //Full House
+    let check2 = false;
+    let check3 = false;
+    if (numOfOnes === 2 || numOfTwos === 2 || numOfThrees === 2 || numOfFours === 2 || numOfFives === 2 || numOfSixes === 2) {
+      check2 = true;
+    }
+
+    if (numOfOnes === 3 || numOfTwos === 3 || numOfThrees === 3 || numOfFours === 3 || numOfFives === 3 || numOfSixes === 3) {
+      check3 = true;
+    }
+
+    if (check2 === true && check3 === true) {
+      document.getElementById("fullhouseScore").innerHTML = fullHouse;
+    }
+    else {
+      document.getElementById("fullhouseScore").innerHTML = "0";
+    }
+
+    //Small Straight
+    if (finalRoll.includes(1) && finalRoll.includes(2) && finalRoll.includes(3) && finalRoll.includes(4)) {
+      document.getElementById("smallstraightScore").innerHTML = smallStraight;
+    }
+    else if (finalRoll.includes(2) && finalRoll.includes(3) && finalRoll.includes(4) && finalRoll.includes(5)) {
+      document.getElementById("smallstraightScore").innerHTML = smallStraight;
+    }
+    else {
+      document.getElementById("smallstraightScore").innerHTML = "0";
+    }
+
+    //Large Straight
+    if (finalRoll.includes(1) && finalRoll.includes(2) && finalRoll.includes(3) && finalRoll.includes(4) && finalRoll.includes(5)) {
+      document.getElementById("largestraightScore").innerHTML = largeStraight;
+    }
+    else {
+      document.getElementById("largestraightScore").innerHTML = "0";
+    }
+
+    //Chance
+    document.getElementById("chanceScore").innerHTML = diceTotal;
+
+    //Yahtzee
+    if (numOfOnes === 5) {
+      document.getElementById("yahtzeeScore").innerHTML = yahtzee;
+    }
+    if (numOfTwos === 5) {
+      document.getElementById("yahtzeeScore").innerHTML = yahtzee;
+    }
+    if (numOfThrees === 5) {
+      document.getElementById("yahtzeeScore").innerHTML = yahtzee;
+    }
+    if (numOfFours === 5) {
+      document.getElementById("yahtzeeScore").innerHTML = yahtzee;
+    }
+    if (numOfFives === 5) {
+      document.getElementById("yahtzeeScore").innerHTML = yahtzee;
+    }
+    if (numOfSixes === 5) {
+      document.getElementById("yahtzeeScore").innerHTML = yahtzee;
+    }
+    else {
+      document.getElementById("yahtzeeScore").innerHTML = "0";
+    }
+  }
+  //^^^^^^^^^^^^^End of Scoring Section^^^^^^^^^^^^^^^^^^^^^
 }
 
 function diceHold(diceName) {
@@ -143,8 +285,18 @@ function displayDie(value, id) {
 }
 
 
-
-
 /* Homework
-1. total score after 3 rolls
+Reset button works.
+Game totals up score. However, will not include held dice in score.
+
+1. at end of each round, all possible scores should be displayed in a color that stands out.
+2. when I click which score I want to choose, the score turns black, other scores disappear, remaining rolls resets to 3, 
+dice faces disappear, dice are able to be rolled again.
+3. when all scores are filled in either the upper or lower section, the section total is filled in
+4. if upper total is greater than 63, and 35 point bonus is added on
+5. when both upper and lower sections are filled in and totaled, the final total at the bottom is calculated and filled in
+6. shorten bottom of playing board up to bottom of score area
+7. center dice in playing area
+8. instead of blue dice, create a held dice area at bottom of playing area
+9. when dice are rolled, they move upwards and tilt, rotate
 */
